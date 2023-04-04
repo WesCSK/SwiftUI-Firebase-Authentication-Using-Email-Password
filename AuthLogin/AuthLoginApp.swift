97e0fb330ee1bb4fe39e5f8c86bc274035908173
Swee Kwang <sweekwang94@gmail.com>
2023-04-04 09:20:07 +0800

    Initial Commit

---
+++ AuthLogin/AuthLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthLoginApp.swift
+//  AuthLogin
+//
+//  Created by Swee Kwang Chua on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct AuthLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
