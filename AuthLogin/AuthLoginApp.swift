f03f5db95a56ac5d697aa7ff729555e73c1265a4
Swee Kwang <sweekwang94@gmail.com>
2023-04-13 22:29:03 +0800

    Completed create account logic

--- AuthLogin/AuthLoginApp.swift
+++ AuthLogin/AuthLoginApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct AuthLoginApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             RouteView()
--- AuthLogin/Screen/RegisterScreen.swift
+++ AuthLogin/Screen/RegisterScreen.swift
@@ -7,11 +7,15 @@
 
 import SwiftUI
 import SwiftRUI
+import FirebaseAuth
 
 struct RegisterScreen: View {
     @Binding var navpath: NavigationPath
     
     @State var email: String = ""
+    @State var password: String = ""
+    @State var isError: Bool = false
+    @State var message: String = ""
     
     var body: some View {
         ZStack {
@@ -32,18 +36,26 @@ struct RegisterScreen: View {
                     .font(.subheadline)
                     .foregroundColor(.gray)
                 
+                if (!message.isEmpty) {
+                    Text(message)
+                        .padding(.top, 0)
+                        .font(.subheadline)
+                        .foregroundColor(isError ? .red : .blue)
+                }
+                
                 RUITextfield(text: $email,
                              placeholder: "Email Address")
                 .padding(.top, 38)
+                .keyboardType(.emailAddress)
                 
-                RUITextfield(text: $email,
+                RUITextfield(text: $password,
                              placeholder: "Password",
                              isSecure: true)
                 .padding(.vertical)
                 
                 
                 RUIRoundedCornerTextButton(text: "Sign up") {
-                    navpath.append(Routes.fogotpwd)
+                    createUser()
                 }
                 .padding(.top)
                 
@@ -53,6 +65,34 @@ struct RegisterScreen: View {
             } // HStack
             .padding(.horizontal, 32)
             .padding(.vertical)
+            .onAppear {
+                print(_email.projectedValue)
+                print(_email.wrappedValue)
+            }
+        }
+    }
+    
+    private func createUser() {
+        // TODO: Verification Check
+        message = ""
+        isError = false
+        
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            
+            guard let result = authResult,
+                  error == nil else {
+                isError.toggle()
+                message = error?.localizedDescription ?? "An error has occured"
+                return
+            }
+            
+            let user = result.user
+            
+            // Send email verification to the email address of the user.
+            user.sendEmailVerification { _ in
+                message = "We has sent an email. Follow the instruction to verify your email address."
+            }
+            
         }
     }
 }
